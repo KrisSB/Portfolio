a0cff83bee5ae1b19305025081b71e3b1b9a6710
Keith LaValley <kelava91@gmail.com>
2018-12-31 17:50:46 -0500

    Updates

---
+++ precache-manifest.97d9796d8636e89417132a4663ddc261.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "350f8ce0eb0a59dcd43edc9c867deb93",
+    "url": "/Portfolio/static/media/projects.350f8ce0.jpg"
+  },
+  {
+    "revision": "6d93996d9167112c7e0d4c48f6e99362",
+    "url": "/Portfolio/static/media/home.6d93996d.jpg"
+  },
+  {
+    "revision": "037fece79744fdf1a50dc9b5039a7985",
+    "url": "/Portfolio/static/media/family.037fece7.jpg"
+  },
+  {
+    "revision": "d6aa77c844ef327aa37c7ba0f43d0b9b",
+    "url": "/Portfolio/static/media/contact.d6aa77c8.jpg"
+  },
+  {
+    "revision": "c77bed2d45b82a4b75da85ca0f04a8d8",
+    "url": "/Portfolio/static/media/about.c77bed2d.jpg"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "bfc38afa01d190e234fb",
+    "url": "/Portfolio/static/js/main.bfc38afa.chunk.js"
+  },
+  {
+    "revision": "8b3c6203bbf48dd9e85b",
+    "url": "/Portfolio/static/js/1.8b3c6203.chunk.js"
+  },
+  {
+    "revision": "bfc38afa01d190e234fb",
+    "url": "/Portfolio/static/css/main.19f7c485.chunk.css"
+  },
+  {
+    "revision": "2a981660c5a0f75dafe09039d8cda915",
+    "url": "/Portfolio/index.html"
+  }
+];
